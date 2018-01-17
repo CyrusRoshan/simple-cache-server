2fb437ea7522f3ea8d3eaa922499bc62fd1b27d1
Cyrus Roshan <cyrusroshan123@gmail.com>
2018-01-17 09:24:20 -0600

    Add really basic tests to make sure the client works, add scaffolding for more main tests.

--- main_test.go
+++ main_test.go
@@ -1,7 +1,62 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
 
-func TestMain(t *testing.T) {
+	"github.com/go-redis/redis"
+)
+
+var redisClient *redis.Client
+
+func TestRedisSetup(t *testing.T) {
+	conf := getConfig()
+
+	var pong string
+	redisClient, pong = connectRedis(conf.RedisAddress)
+
+	if pong != "PONG" {
+		t.Fail()
+	}
+}
+
+func TestRedisKeys(t *testing.T) {
+	redisClient.Set("KEY1", "VAL1", time.Hour)
+	redisClient.Set("KEY2", "VAL2", time.Hour)
+	redisClient.Set("KEY3", "VAL3", time.Hour)
+
+	if redisClient.Get("KEY1").Val() != "VAL1" ||
+		redisClient.Get("KEY2").Val() != "VAL2" ||
+		redisClient.Get("KEY3").Val() != "VAL3" {
+		t.Fail()
+	}
+
+	redisClient.Set("KEY1", "VAL4", time.Hour)
+	redisClient.Set("KEY2", "VAL5", time.Hour)
+
+	if redisClient.Get("KEY1").Val() != "VAL4" ||
+		redisClient.Get("KEY2").Val() != "VAL5" ||
+		redisClient.Get("KEY3").Val() != "VAL3" {
+		t.Fail()
+	}
+}
+
+func TestProxy(t *testing.T) {
+
+}
+
+func TestProxyKeys(t *testing.T) {
+
+}
+
+func TestProxyCache(t *testing.T) {
+
+}
+
+func TestConcurrentClients(t *testing.T) {
+
+}
+
+func TestAsyncClients(t *testing.T) {
 
 }
