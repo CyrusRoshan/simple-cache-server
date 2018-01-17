22628d9a0fcb274ae113fe691fef7ae6d00cedbf
Cyrus Roshan <cyrusroshan123@gmail.com>
2018-01-16 19:26:14 -0600

    Get redis working, change redis default port, clean up main.go into separate functions.

--- main.go
+++ main.go
@@ -4,13 +4,29 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/go-redis/redis"
+
 	"github.com/CyrusRoshan/simple-cache-server/config"
 )
 
 const CONFIGFILE = "config.toml"
 
 func main() {
-	// read config
+	conf := getConfig()
+	fmt.Println("Config read", *conf)
+	fmt.Println()
+
+	// log success
+	redisClient, pong := connectRedis(conf.RedisAddress)
+	fmt.Println("Successfully connected to redis, with a ping for a", pong)
+	fmt.Println(redisClient)
+	fmt.Println()
+
+	// start proxy with config info
+	// log proxy running on info
+}
+
+func getConfig() *config.Config {
 	configFile := os.Getenv("CONFIGFILE")
 	if configFile == "" {
 		configFile = CONFIGFILE
@@ -21,11 +37,20 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("Config read", *conf)
+	return conf
+}
 
-	// connect to redis
-	// log success
+func connectRedis(address string) (*redis.Client, string) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     address,
+		Password: "",
+		DB:       0,
+	})
 
-	// start proxy with config info
-	// log proxy running on info
+	pong, err := client.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+
+	return client, pong
 }
